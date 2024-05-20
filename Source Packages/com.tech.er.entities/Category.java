8dc704de885241fa57e3aa1c07e632c48e3524e1
DevanshKuchekar <109858857+DevanshKuchekar@users.noreply.github.com>
2024-05-20 18:12:20 +0530

    Create Category.java

---
+++ Source Packages/com.tech.er.entities/Category.java
@@ -0,0 +1,35 @@
+package com.tech.blog.entities;
+
+public class Category {
+    private int cid;
+    private String name;
+
+    public Category(int cid, String name) {
+        this.cid = cid;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
